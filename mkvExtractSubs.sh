0c93e977649019b94df1a2d58ce3cd40293d1471
Ethan Jansen <ethancjansen@gmail.com>
2025-01-01 15:03:04 -0800

    moved destination to last argument, forced mkv file check

--- mkvExtractSubs.sh
+++ mkvExtractSubs.sh
@@ -6,17 +6,17 @@ shopt -s nullglob
 
 ################### Global Variables ######################
 
-
+Destination=
 
 ################## Functions #############################
 
 Usage(){
-  echo "Extract all subtitle streams from input media files."
+  echo "Extract all subtitle streams from input mkv files."
   echo "Input an array of files or directories to check."
-  echo "Directories are depth-1 searched."
+  echo "Directories are depth-1 searched for mkv files."
   echo "Depends: mkvtoolnix."
   echo
-  echo "Syntax: ffmpegHash.sh <destination folder> <file1> [<file2> <file3>...]"
+  echo "Syntax: ffmpegHash.sh <file1.mkv> [<file2.mkv> <file3.mkv>...] <destination folder>"
   echo "Options:"
   echo "-h      Print this help message."
 } 
@@ -24,26 +24,33 @@ Usage(){
 ############### Main ####################
 
 # Test inputs
-
 if [ $# -lt 2 ] || (printf -- '%s\n' "$@" | grep -Fxq -- '-h'); then
   Usage
   exit 1
 fi
 
+# get the destination folder
+Destination="${*: -1}"
+if ! [ -d "$Destination" ]; then
+  echo -e "\033[31m$Destination is not a valid destination!\033[0m" >&2
+  exit 1
+fi
+echo "Using destination: $Destination"
+
 # read arguments and hash files
-for arg; do
-  if [ -f "$arg" ]; then # Handle files
+for arg in "${@:1:$#}"; do
+  if [ -f "$arg" ] && [[ "$arg" =~ .*\.mkv$ ]]; then # Handle files
     echo "Processing file: $arg"
   elif [ -d "$arg" ]; then # Handle directories
     # get all files/directories at depth=1, but omit directories
-    argDir=("$arg"/*)
+    argDir=("$arg"/*.mkv)
     for argFile in "${argDir[@]}"; do
       if [ -f "$argFile" ]; then
         echo "Processing directory file: $argFile"
       fi
     done
   else  # No valid file/directory
-    echo -e "\033[31mFile/Directory not found: $arg\033[0m" >&2
+    echo -e "\033[31mInvalid file/directory: $arg\033[0m" >&2
     # exit 1 # Do not exit, just warn and continue
   fi
 done
