4d246b94fdaf7d5d4743e640d316387d5c6c1551
Ethan Jansen <ethancjansen@gmail.com>
2025-01-02 17:49:21 -0800

    alway paginate with bat, cleaned up input handling

--- ffmpegMetadata.sh
+++ ffmpegMetadata.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 # Output specific information about a media file using ffmpeg.
-# Requires `ffmpeg/ffmprobe` (and `bat` if paginating output).
+# Requires `ffmpeg/ffmprobe` and `bat` to paginate output.
 # By: Ethan Jansen
 
-# Need to check for input file name
 Usage(){
   echo "Output specific information about a media file using ffmpeg/ffprobe."
-  echo "Depends: ffmpeg/ffmprobe (and optionally bat)."
+  echo "Depends: ffmpeg/ffmprobe and bat."
   echo
-  echo "Syntax: ffmpegMetadata.sh <file> [-p]"
+  echo "Syntax: ffmpegMetadata.sh <file> "
   echo "Options:"
-  echo "-p      Paginate output using bat."
   echo "-h      Print this help message."
 } 
 
@@ -19,45 +17,15 @@ Probe(){
 }
 
 ############### Main ####################
-PAGINATE=1
-PARAMS=() # index 0 should contain file name
-
-while [ $# -gt 0 ]; do
-  while getopts "ph" opt; do
-    case $opt in
-      h) # display help
-        Usage
-        exit 1;;
-      p) # paginate output
-        PAGINATE=0;;
-      \?) # invalid
-        echo "Error: Invalid argument"
-        exit 1;;
-    esac
-  done
-  if [[ ${OPTIND:-0} -gt 0 ]]; then
-    shift $((OPTIND-1))
-  fi
-
-  while [ $# -gt 0 ] && ! [[ "$1" =~ ^- ]]; do
-    PARAMS=("${PARAMS[@]}" "$1")
-    shift
-  done
-done
-
-# Not perfect input handling, but whatever
-if [ ${#PARAMS[@]} -ne 1 ]; then
+# check input
+if [ $# -ne 1 ] || (printf -- '%s\n' "$@" | grep -Fxw -- '-h'); then
   Usage
   exit 1
 fi
 
-if [ ! -f "${PARAMS[0]}" ]; then
-  echo "Error: File not found!"
+if [ ! -f "$1" ]; then
+  echo -e "\033[31mError: File $1 not found!\033[0m"
   exit 1
 fi
 
-if [[ "$PAGINATE" -eq 0 ]]; then
-  Probe "${PARAMS[0]}" | bat --language json --theme TwoDark --file-name "$(basename "${PARAMS[0]}")"
-else
-  Probe "${PARAMS[0]}"
-fi
+Probe "$1" | bat --language json --theme TwoDark --file-name "$(basename "$1")"
